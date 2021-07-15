dc24b2fa6373f0e30bf42ca0fcf1af1776247938
deveassy <xjfmwld@gmail.com>
2021-07-15 21:12:37 +0900

    레이아웃 초안 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 사이드바 레이아웃 */}
+      <div>
+        {/* 검색 영역 */}
+        <div></div>
+        {/* 메뉴 영역 */}
+        <div>
+          <ul>
+            <li>진행상황</li>
+            <li>동기화</li>
+            <li>기타</li>
+          </ul>
+        </div>
+      </div>
+      {/* 메인 레이아웃 */}
+      <div>
+        {/* 타이틀 영역 */}
+        <div>
+          <h1>타이틀</h1>
+        </div>
+        {/* 본문 영역 */}
+        <div>
+          <ul>
+            <li>
+              <div>할 일 1</div>
+            </li>
+            <li>
+              <div>할 일 2</div>
+            </li>
+            <li>
+              <div>할 일 3</div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
